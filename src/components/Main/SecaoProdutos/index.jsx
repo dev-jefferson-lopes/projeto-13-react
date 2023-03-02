7d39490fed83aa1d0a5713865537f57500f6e020
Jefferson Lopes <dev.jefferson.lopes@gmail.com>
2023-03-02 12:44:24 -0300

    Inserindo os elementos da SecaoProdutos

--- src/components/Main/SecaoProdutos/index.jsx
+++ src/components/Main/SecaoProdutos/index.jsx
@@ -1,9 +1,46 @@
 import React from "react";
 
-export default function SecaoProdutos(){
-    return(
+export default function SecaoProdutos() {
+    return (
         <section>
-            <h5>SecaoProdutos</h5>
+            <div>
+                <h2>Nossos produtos</h2>
+                <p>Trabalhamos com óculos de grau, óculos de sol, lentes transition nos modelos masculino, feminino e infantil.</p>
+                <p>Todos os nossos preços são acessíveis e contam com a melhor qualidade do mercado.</p>
+
+                <div>
+                    <div>
+                        <h3>Óculos de grau</h3>
+                        <img src="/public/assets/oculos01.png" alt="Óculos de grau" />
+                        <p>R$ 500,00</p>
+                    </div>
+                    <div>
+                        <h3>Óculos transition</h3>
+                        <img src="/public/assets/oculos02.png" alt="Óculos de grau" />
+                        <p>R$ 750,00</p>
+                    </div>
+                    <div>
+                        <h3>Óculos de sol</h3>
+                        <img src="/public/assets/oculos03.png" alt="Óculos de grau" />
+                        <p>R$ 700,00</p>
+                    </div>
+                    <div>
+                        <h3>Óculos infantil</h3>
+                        <img src="/public/assets/oculos04.png" alt="Óculos de grau" />
+                        <p>R$ 500,00</p>
+                    </div>
+                </div>
+
+                <p>Todos os nossos produtos incluem:</p>
+                <ul>
+                    <li>Garantia de 1 ano</li>
+                    <li>Manutenção preventiva</li>
+                    <li>Descontos especiais na compra da segunda unidade</li>
+                    <li>Flexibilidade de pagamento</li>
+                </ul>
+
+
+            </div>
         </section>
     )
 }
