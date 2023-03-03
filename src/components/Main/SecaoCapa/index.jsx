c95e6308fdc948047dade0a0647828da3953e50a
Jefferson Lopes <dev.jefferson.lopes@gmail.com>
2023-03-03 14:26:11 -0300

    tornando responsivo

--- src/components/Main/SecaoCapa/index.jsx
+++ src/components/Main/SecaoCapa/index.jsx
@@ -3,10 +3,12 @@ import "./index.scss"
 export default function SecaoCapa() {
     return (
         <section className="secao-capa">
-            <div className="section-container">
-                <p>Preços baixos e</p>
-                <h1>Óculos de grau e de sol</h1>
-                <p>Você só encontra aqui</p>
+            <div className="gradient">
+                <div className="section-container">
+                    <p>Preços baixos e</p>
+                    <h1>Óculos de grau e de sol</h1>
+                    <p>Você só encontra aqui</p>
+                </div>
             </div>
         </section>
     )
