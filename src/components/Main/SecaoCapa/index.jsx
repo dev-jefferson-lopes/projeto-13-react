a36f36797dcc91ac463400794970ba43fc6cf275
Jefferson Lopes <dev.jefferson.lopes@gmail.com>
2023-03-03 11:31:15 -0300

    Inserindo o estilo da seção capa

--- src/components/Main/SecaoCapa/index.jsx
+++ src/components/Main/SecaoCapa/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import "./index.scss"
 export default function SecaoCapa() {
     return (
-        <section>
-            <div>
+        <section className="secao-capa">
+            <div className="section-container">
                 <p>Preços baixos e</p>
                 <h1>Óculos de grau e de sol</h1>
                 <p>Você só encontra aqui</p>
